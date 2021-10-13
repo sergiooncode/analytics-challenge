b2091852dac1a192c83e36ea8be684edabae2a78
Sergio Perez <sperez4mba@gmail.com.com>
2021-10-13 18:21:01 +0200

    Refactor aggregation data repository

--- src/metrics/aggregation/infrastructure/persistence/sqlalchemy/sqlalchemy_metric_aggregation_repository.py
+++ src/metrics/aggregation/infrastructure/persistence/sqlalchemy/sqlalchemy_metric_aggregation_repository.py
@@ -1,8 +1,12 @@
+import statistics
 from datetime import datetime
+
+from sqlalchemy.sql import func
 
 from src.metrics.aggregation.domain.metric_aggregation_repository import (
     MetricAggregationRepository,
 )
+from src.metrics.core.persistence.models import Metric
 from src.metrics.core.persistence.models.metric_record import MetricRecord
 
 
@@ -10,30 +14,28 @@ class SqlalchemyMetricAggregationRepository(MetricAggregationRepository):
     def aggregate(
         self, metric_name: str, min_date: datetime, max_date: datetime
     ) -> float:
-        children_metrics_value_sum = 0
-        number_of_metrics_to_average = 1
-        metric_value = 0.0
-        metric_tree = (
-            MetricRecord.query
+        given_metric_metric_tree = MetricRecord.query.filter(
+            MetricRecord.metric.has(name=metric_name),
+            MetricRecord.created_at >= min_date,
+            MetricRecord.created_at <= max_date,
+        ).all()
+
+        given_metric_children_tree = (
+            MetricRecord.query.join(Metric)
             .filter(
-                MetricRecord.metric.has(name=metric_name),
+                Metric.parent_metric_id.in_(
+                    (Metric.query.filter_by(name=metric_name).first().id,)
+                ),
                 MetricRecord.created_at >= min_date,
                 MetricRecord.created_at <= max_date,
             )
             .all()
         )
-        if metric_tree:
-            metric_value = metric_tree[0].value
-            children_metrics_dict = metric_tree[0].metric.children_metrics
-            children_metrics_ids = (
-                children_metrics_dict[children_metric_name].id
-                for children_metric_name in children_metrics_dict
-            )
-            children_metric_records = MetricRecord.query.filter(MetricRecord.metric_id.in_(children_metrics_ids)).all()
-            for children_metric_record in children_metric_records:
-                children_metrics_value_sum += children_metric_record.value
-            if children_metrics_value_sum != 0:
-                number_of_metrics_to_average += len(children_metric_records)
-        return (
-            metric_value + children_metrics_value_sum
-        ) / number_of_metrics_to_average
+
+        given_metric_children_tree_values = [o.value for o in given_metric_children_tree]
+        given_metric_metric_tree_values = [o.value for o in given_metric_metric_tree]
+        given_metric_children_tree_values.extend(
+            given_metric_metric_tree_values
+        )
+
+        return statistics.mean(given_metric_children_tree_values)
